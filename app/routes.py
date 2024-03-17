8d9a116ee7b258a41e8da33b7421efedb83650d1
MeloJR <jrmeloafrf@gmail.com>
2024-03-17 16:58:10 -0300

    incluindo mais funcionalidades

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,9 @@ from app import app, db, bcrypt
 from app.forms import FormLogin, FormCriarConta, FormEditarPerfil
 from app.models import Usuario
 from flask_login import login_user, logout_user, current_user, login_required
-
-
-lista_usuarios = ['lira', 'Joao', 'Alon', 'Alessandra', 'Amanda']
+import secrets
+import os
+from PIL import Image
 
 @app.route("/")
 def home():
@@ -20,6 +20,7 @@ def contato():
 @app.route("/usuarios")
 @login_required
 def usuarios():
+    lista_usuarios = Usuario.query.all()
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
 @app.route('/login', methods=["GET", "POST"])
@@ -70,10 +71,45 @@ def criar_post():
     return render_template('criarpost.html')
 
 
+def salvar_imagem(imagem):
+    codigo = secrets.token_hex(8)
+    nome, extensao = os.path.splitext(imagem.filename)
+    nome_arquivo = nome + codigo + extensao
+    caminho_completo = os.path.join(
+        app.root_path, 'static/fotos_perfil', nome_arquivo)
+    tamanho = (400, 400)
+    imagem_reduzida = Image.open(imagem)
+    imagem_reduzida.thumbnail(tamanho)
+    imagem_reduzida.save(caminho_completo)
+    return nome_arquivo
+
+
+def atualizar_cursos(form):
+    lista_cursos = []
+    for campo in form:
+        if 'curso_' in campo.name:
+            if campo.data:
+                lista_cursos.append(campo.label.text)
+    return ';'.join(lista_cursos)
+
+
 @app.route('/perfil/editar', methods=['GET', 'POST'])
 @login_required
 def editar_perfil():
     form = FormEditarPerfil()
+    if form.validate_on_submit():
+        current_user.email = form.email.data
+        current_user.username = form.username.data
+        if form.foto_perfil.data:
+            nome_imagem = salvar_imagem(form.foto_perfil.data)
+            current_user.foto_perfil = nome_imagem
+        current_user.cursos = atualizar_cursos(form)
+        db.session.commit()
+        flash('Perfil atualizado com Sucesso', 'alert-success')
+        return redirect(url_for('perfil'))
+    elif request.method == "GET":
+        form.email.data = current_user.email
+        form.username.data = current_user.username
     foto_perfil = url_for(
         'static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
     return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
